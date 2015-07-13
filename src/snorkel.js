c13f57cffe1da148fec81ab030c4bb0aedb9bed8
Ed Moore <eddie.moore@gmail.com>
2015-07-13 09:49:29 +0800

    cleaned up for loops

--- src/snorkel.js
+++ src/snorkel.js
@@ -18,21 +18,21 @@ export default {
   },
   off: function (event_name, callback) {
     if (listeners.hasOwnProperty(event_name)) {
-      for (let i = 0, l = listeners[event_name].length; i < l; i++) {
-        if (listeners[event_name][i] === callback) {
+      listeners[event_name].forEach((val, i) => {
+        if (val === callback) {
           listeners[event_name].splice(i, 1)
           return
         }
-      }
+      })
     }
   },
   emit: function (event_name) {
     if (listeners.hasOwnProperty(event_name)) {
       const callbacks = listeners[event_name]
       const args = Array.prototype.slice.call(arguments, 1)
-      for (let i = 0, l = callbacks.length; i < l; i++) {
-        callbacks[i].apply(this, args)
-      }
+      callbacks.forEach(val => {
+        val.apply(this, args)
+      })
     }
   },
   clear: function () {
