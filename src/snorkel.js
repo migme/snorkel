7aacee0b255001d3d463d147448efa5d05173199
Ed Moore <eddie.moore@gmail.com>
2015-07-13 09:31:54 +0800

    Cleaning up

--- src/snorkel.js
+++ src/snorkel.js
@@ -1,34 +1,34 @@
 let listeners = {}
 export default {
-  on: function (evt_name, callback) {
-    if (!Object.prototype.hasOwnProperty.call(listeners, evt_name)) {
-      listeners[evt_name] = []
+  on: function (event_name, callback) {
+    if (!listeners.hasOwnProperty(event_name)) {
+      listeners[event_name] = []
     }
-    listeners[evt_name].push(callback)
+    listeners[event_name].push(callback)
   },
-  once: function (evt_name, callback) {
+  once: function (event_name, callback) {
     const once = function () {
       try {
         return callback.apply(this, arguments)
       } finally {
-        this.off(evt_name, once)
+        this.off(event_name, once)
       }
     }
-    this.on(evt_name, once)
+    this.on(event_name, once)
   },
-  off: function (evt_name, callback) {
-    if (listeners.hasOwnProperty(evt_name)) {
-      for (let i = 0, l = listeners[evt_name].length; i < l; i++) {
-        if (listeners[evt_name][i] === callback) {
-          listeners[evt_name].splice(i, 1)
+  off: function (event_name, callback) {
+    if (listeners.hasOwnProperty(event_name)) {
+      for (let i = 0, l = listeners[event_name].length; i < l; i++) {
+        if (listeners[event_name][i] === callback) {
+          listeners[event_name].splice(i, 1)
           return
         }
       }
     }
   },
-  emit: function (evt_name) {
-    if (listeners.hasOwnProperty(evt_name)) {
-      const callbacks = listeners[evt_name]
+  emit: function (event_name) {
+    if (listeners.hasOwnProperty(event_name)) {
+      const callbacks = listeners[event_name]
       const args = Array.prototype.slice.call(arguments, 1)
       for (let i = 0, l = callbacks.length; i < l; i++) {
         callbacks[i].apply(this, args)
